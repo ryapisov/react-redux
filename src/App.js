f03f31f50fe9e530ff878a845b18a71b287270c2
Alexey V.Ryapisov <tealikes@yandex.ru>
2020-09-24 00:17:12 +0300

    Add redux

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React, { Component } from "react";
+import { connect } from 'react-redux'
 
 class App extends Component {
+
   render() {
+    console.log(this.props)
     return (
       <div>
-        <h1>My React App!!!</h1>
+        <div><input value={this.props.one} placeholder="one" /></div>
+        <div><input value={this.props.two} placeholder="two" /></div>
       </div>
     );
   }
 }
 
-export default App;
+// данные из state в компонент
+const mapStateToProps = (state) => {
+  return {
+    one: state.oneInput,
+    two: state.twoInput
+  }
+}
+
+export default connect(mapStateToProps, null)(App)
--- src/index.js
+++ src/index.js
@@ -1,6 +1,37 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 import App from "./App.js"
 import './style.scss'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const initialState = {
+  oneInput: 'Илья',
+  twoInput: 'Балашов'
+}
+
+const ACTION_CHANGE_FIRST_NAME = 'ACTION_CHANGE_FIRST_NAME'
+const ACTION_CHANGE_SECOND_NAME = 'ACTION_CHANGE_SECOND_NAME'
+
+const actionChangeFirstName = {
+  type: ACTION_CHANGE_FIRST_NAME,
+  payload: null
+}
+
+const actionChangesSecondName = {
+  type: ACTION_CHANGE_SECOND_NAME,
+  payload: null
+}
+
+const rootReduser = (state = initialState, action) => {
+  return state
+}
+
+const store = createStore(rootReduser)
+
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>, document.getElementById('root')
+)
